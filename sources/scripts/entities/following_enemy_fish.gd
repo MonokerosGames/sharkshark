a33cba26b2e8f53beaf7fd54fff58fb49ff2ca61
Non0w <arnaud.cojez@etudiant.univ-lille1.fr>
2017-05-14 00:32:10 +0200

    add stupid following enemy fish

---
+++ sources/scripts/entities/following_enemy_fish.gd
@@ -0,0 +1,18 @@
+extends "res://scripts/entities/enemy_fish.gd"
+
+export var swim_speed = 100
+export (NodePath) var target_path
+
+func _ready():
+	
+	if target_path == null:
+		print("ERROR: following_enemy_fish (", self.get_path(), ") target has not been assigned.")
+	else:
+		set_fixed_process(true)
+		
+func _fixed_process(delta):
+	if has_node(target_path):
+		var target = get_node(target_path)
+		if target != null:
+			var direction = (target.get_pos() - get_pos()).normalized()
+			move(swim_speed * direction * delta)
